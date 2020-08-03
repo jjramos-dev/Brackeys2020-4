2192da2be05dce4165003e45b1c1df9c5413f84d
Juanjo Ramos <jjramos@ugr.es>
2020-08-04 01:01:40 +0200

    Exit door and killing ice added.

---
+++ Game/Environment/ExitDoor.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+signal entered_door()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Door_body_entered(body):
+	emit_signal("entered_door")
