abf22b2eab28330c0c1ea5e75a06ed60bfdbdf4f
raformatico <raformatico@raformatico.com>
2020-08-03 20:04:09 +0200

    Two options for puppet jump with player on it
    None of them working :(

--- Game/Puppets/PuppetJump/PuppetJump.gd
+++ Game/Puppets/PuppetJump/PuppetJump.gd
@@ -5,15 +5,27 @@ func _ready():
 	discharge_rate = 8
 
 func _on_discharge():
+	if energy>0:
+		direction = 1
+	elif direction > 0.01:
+		direction *= stop_speed
+	else:
+		direction = 0
+	velocity.y = -speed * direction
+	velocity = move_and_slide(velocity,Vector2.UP)
+	"""var pre_vel : Vector2
 	if max_energy_charged > 0:
 		print(max_energy_charged)
 		jump(max_energy_charged)
 		max_energy_charged = 0
+	pre_vel = velocity
+	velocity = move_and_slide(velocity,Vector2.UP)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider is Player:
+			print("I collided with ", collision.collider.name)"""
 	
-	velocity = move_and_slide(velocity)
-	"""var col = move_and_collide(gravity_speed)
-	if col is Player:
-		col.move_and_collide(gravity_speed)"""
+
 
 func jump(jump_mult : int) -> void:
 	jump_height = jump_height_step * jump_mult
@@ -21,7 +33,9 @@ func jump(jump_mult : int) -> void:
 	jump_force = gravity * time_jump_apex
 	velocity.y = -jump_force
 	
-	
+func jump2(jump_mult : int) -> void:
+	pass
+
 func apply_charge():
 	if energy > max_energy_charged:
 		max_energy_charged = energy
