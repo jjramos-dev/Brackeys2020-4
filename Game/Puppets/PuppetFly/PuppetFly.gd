c7e08e160bbadf6f72c34b3dd6da1f3f88c12610
raformatico <raformatico@raformatico.com>
2020-08-05 01:32:37 +0200

    Puppet Fly sprite and test scene

--- Game/Puppets/PuppetFly/PuppetFly.gd
+++ Game/Puppets/PuppetFly/PuppetFly.gd
@@ -10,3 +10,26 @@ func apply_charge():
 		direction = 0
 	velocity.x = speed * direction
 	velocity = move_and_slide(velocity,Vector2.UP)
+
+func _physics_process(delta):
+
+	._physics_process(delta)	
+	
+#	if recharging:
+#		state=STATE.RECHARGING
+#	elif energy>0:
+#		state=STATE.DISCHARGING
+#	else:
+#		state=STATE.IDLE
+	#Esto debería estar en el genérico no?
+	match state:
+		STATE.IDLE:
+			$AnimationPlayer.play("idle")
+			
+		STATE.DISCHARGING:
+			#apply_charge()
+			$AnimationPlayer.play("discharging")
+			
+		STATE.RECHARGING:
+			$AnimationPlayer.play("recharging")
+			#recharge(delta*recharge_rate)
