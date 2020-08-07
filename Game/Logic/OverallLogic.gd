8b801d7f828e0779059635ce77c9937fcf4e6baf
raformatico <raformatico@raformatico.com>
2020-08-07 20:36:56 +0200

    Stage 13 finished

--- Game/Logic/OverallLogic.gd
+++ Game/Logic/OverallLogic.gd
@@ -15,7 +15,19 @@ export var transitions= {
 		"door-2.2": ["res://Levels/Stage4/Stage4.tscn","door-4.1"],
 		"door-3.1": ["res://Levels/Stage1/Stage1.tscn","door-1.2"],
 		"door-4.1": ["res://Levels/Stage2/Stage2.tscn","door2.2"],
-		"door-4.2":["res://Levels/Stage6/Stage6.tscn","door-6.1"],
+		"door-4.2":["res://Levels/Stage11/Stage11.tscn","door-11.1"],
+		"door-5.1":["res://Levels/Stage7/Stage7.tscn","door-7.3"],
+		"door-7.1":["res://Levels/Stage15/Stage15.tscn","door-15.2"],
+		"door-7.2":["res://Levels/Stage15/Stage17.tscn","door-17.1"],
+		"door-7.3":["res://Levels/Stage5/Stage5.tscn","door-5.1"],
+		"door-9.1":["res://Levels/Stage11/Stage11.tscn","door-11.2"],
+		"door-9.2":["res://Levels/Stage13/Stage13.tscn","door-13.1"],
+		"door-11.1":["res://Levels/Stage4/Stage4.tscn","door-4.2"],
+		"door-11.2":["res://Levels/Stage9/Stage9.tscn","door-9.1"],
+		"door-13.1":["res://Levels/Stage9/Stage9.tscn","door-9.2"],
+		"door-13.2":["res://Levels/Stage15/Stage15.tscn","door-15.1"],
+		"door-15.1":["res://Levels/Stage13/Stage13.tscn","door-13.2"],
+		"door-15.2":["res://Levels/Stage7/Stage7.tscn","door-7.1"],
 		"door-6.1":["res://Levels/Stage4/Stage4.tscn","door-4.2"]}
 
 # Called when the node enters the scene tree for the first time.
