2a08d8d699bac9f1b9766e9c6cbc4b079e399dcd
raformatico <raformatico@raformatico.com>
2020-08-08 01:25:41 +0200

    Enemy can take several hit and makes a flashing

--- Game/Enemy/enemy.gd
+++ Game/Enemy/enemy.gd
@@ -3,6 +3,7 @@ class_name Enemy
 
 var velocity := Vector2.ZERO
 export var speed := 150
+export var max_life := 3
 var direction := 1
 export var direction_to_walk : = 1
 
@@ -10,12 +11,14 @@ export var fall_mult := 2
 export var jump_height := 120
 export var time_jump_apex := 0.4
 
+var life : int
 var gravity : float
 var jump_force : float
 var on_wall := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	life = max_life
 	$AnimationPlayer.play("run")
 	if direction_to_walk == -1:
 		change_direction()
@@ -86,11 +89,31 @@ func _on_proximity_detection_body_entered(body: Node) -> void:
 		if can_be_killed():
 			#Bullet must return
 			SIGNALS.emit_signal("enemy_hit")
-			#Enemy dies
-			enemy_die()
+			#Enemy is hit
+			enemy_hit()
 
-func enemy_die() -> void:
-	queue_free()
+func enemy_hit() -> void:
+	var dir_prev = direction
+	direction = 0
+	life -= 1
+	var red = (max_life - life)/max_life
+	if life <= 0:
+		for i in 16:
+			$Sprite.modulate = Color("6dff0000")
+			yield(get_tree(), "idle_frame")
+			$Sprite.modulate = Color("ff6d6d")
+			yield(get_tree(), "idle_frame")
+			position += Vector2(rand_range(-3,3),rand_range(-3,3))
+		queue_free()
+	else:
+		for i in 4:
+			$Sprite.modulate = Color("6dff0000")
+			yield(get_tree(), "idle_frame")
+			$Sprite.modulate = Color("ff6d6d")
+			yield(get_tree(), "idle_frame")
+			position += Vector2(rand_range(-3,3),rand_range(-3,3))
+		direction = dir_prev
+		$Sprite.modulate = Color("ffffff")
 
 func _on_proximity_detection_body_exited(body: Node) -> void:
 	if body is Player:
